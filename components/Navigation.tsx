1d56ca2fd43905e6623764a0d12217b45aca9555
kingsleyObot <kingsley.obot@sastrify.com>
2023-04-30 18:50:15 +0100

    fetch articles from medium

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,47 +1,63 @@
-import Link from "next/link"
-import React from "react"
-import ThemeSwitch from "@components/ThemeSwitch";
-import {useRouter} from "next/router";
+import Link from 'next/link'
+import React from 'react'
+import ThemeSwitch from '@components/ThemeSwitch'
+import { useRouter } from 'next/router'
 
 const Navigation = () => {
-    const {pathname} = useRouter();
-    return (
-        <div className="sticky top-0 z-20 py-2 md:py-6 md:mb-6 dark:bg-black bg-white">
-            <div className="container px-4 mx-auto lg:max-w-4xl flex items-center justify-between">
-                <div className="items-start flex">
-                    <Link href="/">
-                        <a className={"font-medium tracking-wider transition-colors text-gray-900 dark:hover:text-sky-500 hover:text-sky-500 uppercase dark:text-white"}>
-                            Paul Knulst
-                        </a>
-                    </Link>
-                </div>
-                <div className="items-end flex space-x-5">
-                    <Link href="/">
-                        <a
-                            className={pathname === "/" ? "font-medium tracking-wider transition-colors dark:text-sky-500 text-sky-500 hover:text-sky-500 uppercase dark:text-white" : "font-medium tracking-wider transition-colors text-gray-900 dark:hover:text-sky-500 hover:text-sky-500 uppercase dark:text-white"}
-                        >
-                            About
-                        </a>
-                    </Link>
-                    <Link href={"/blog"}>
-                        <a
-                            className={pathname === "/blog" ? "font-medium tracking-wider transition-colors dark:text-sky-500 text-sky-500 hover:text-sky-500 uppercase dark:text-white" : "font-medium tracking-wider transition-colors text-gray-900 dark:hover:text-sky-500 hover:text-sky-500 uppercase dark:text-white"}
-                        >
-                            Blog
-                        </a>
-                    </Link>
-                    <Link href={"/supportme"}>
-                        <a
-                            className={pathname === "/supportme" ? "font-medium tracking-wider transition-colors dark:text-sky-500 text-sky-500 hover:text-sky-500 uppercase dark:text-white" : "font-medium tracking-wider transition-colors text-gray-900 dark:hover:text-sky-500 hover:text-sky-500 uppercase dark:text-white"}
-                        >
-                            Support Me
-                        </a>
-                    </Link>
-                    <ThemeSwitch/>
-                </div>
-            </div>
+  const { pathname } = useRouter()
+  return (
+    <div className="sticky top-0 z-20 bg-white py-2 dark:bg-black md:mb-6 md:py-6">
+      <div className="container mx-auto flex items-center justify-between px-4 lg:max-w-4xl">
+        <div className="flex items-start">
+          <Link legacyBehavior href="/">
+            <a
+              className={
+                'font-medium uppercase tracking-wider text-gray-900 transition-colors hover:text-sky-500 dark:text-white dark:hover:text-sky-500'
+              }
+            >
+              Paul Knulst
+            </a>
+          </Link>
         </div>
-    )
+        <div className="flex items-end space-x-5">
+          <Link legacyBehavior href="/">
+            <a
+              className={
+                pathname === '/'
+                  ? 'font-medium uppercase tracking-wider text-sky-500 transition-colors hover:text-sky-500 dark:text-sky-500 dark:text-white'
+                  : 'font-medium uppercase tracking-wider text-gray-900 transition-colors hover:text-sky-500 dark:text-white dark:hover:text-sky-500'
+              }
+            >
+              About
+            </a>
+          </Link>
+          <Link legacyBehavior href={'/blog'}>
+            <a
+              className={
+                pathname === '/blog'
+                  ? 'font-medium uppercase tracking-wider text-sky-500 transition-colors hover:text-sky-500 dark:text-sky-500 dark:text-white'
+                  : 'font-medium uppercase tracking-wider text-gray-900 transition-colors hover:text-sky-500 dark:text-white dark:hover:text-sky-500'
+              }
+            >
+              Blog
+            </a>
+          </Link>
+          <Link legacyBehavior href={'/supportme'}>
+            <a
+              className={
+                pathname === '/supportme'
+                  ? 'font-medium uppercase tracking-wider text-sky-500 transition-colors hover:text-sky-500 dark:text-sky-500 dark:text-white'
+                  : 'font-medium uppercase tracking-wider text-gray-900 transition-colors hover:text-sky-500 dark:text-white dark:hover:text-sky-500'
+              }
+            >
+              Support Me
+            </a>
+          </Link>
+          <ThemeSwitch />
+        </div>
+      </div>
+    </div>
+  )
 }
 
-export default Navigation;
+export default Navigation
--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,80 +1,112 @@
 import Parser from 'rss-parser'
-import {useEffect, useState} from "react";
-import SkeletonBlogEntry from "@components/SkeletonBlogEntry";
+import { useEffect, useState } from 'react'
+import SkeletonBlogEntry from '@components/SkeletonBlogEntry'
+
+interface FeedItem {
+  title: string
+  link: string
+  contentSnippet: string
+  description: string
+  image: string
+  pubDate: string
+}
+
+interface CustomFeed {
+  items: FeedItem[]
+}
+
+const extractImageUrl = (description: string): string | undefined => {
+  const startIndex = description.indexOf('https://cdn-images')
+  if (startIndex !== -1) {
+    const endIndex = description.indexOf('" width', startIndex)
+    if (endIndex !== -1) {
+      return description.substring(startIndex, endIndex)
+    }
+  }
+}
 
 const Blog = () => {
-    const [data, setData] = useState([])
-    const [loading, setLoading] = useState(false);
+  const [data, setData] = useState([])
+  const [loading, setLoading] = useState(false)
 
-    const openInNewTab = (url: string): void => {
-        const newWindow = window.open(url, '_blank', 'noopener')
-        if (newWindow) newWindow.opener = null
+  const openInNewTab = (url: string): void => {
+    const newWindow = window.open(url, '_blank', 'noopener')
+    if (newWindow) newWindow.opener = null
+  }
+
+  useEffect(() => {
+    setLoading(true)
+    const getFeeds = async () => {
+      interface CustomFeed {}
+      interface CustomItem {
+        description: string
+      }
+      const parser: Parser<CustomFeed, CustomItem> = new Parser({
+        customFields: {
+          item: ['description'],
+        },
+      })
+      try {
+        const feed = await parser.parseURL('https://medium.com/@kingslifeudo')
+        console.log(`feed: ${feed}`)
+        const data = feed.items.map((item) => ({
+          title: item.title,
+          link: item.link.substring(0, item.link.indexOf('?')),
+          snippet: item.contentSnippet,
+          description: item.description,
+          image: item.description?.substring(
+            item.description.indexOf('https://cdn-images'),
+            item.description.indexOf('" width')
+          ),
+          pubDate: item.pubDate,
+        }))
+        setData(data)
+        setLoading(false)
+      } catch (err) {
+        console.error(err)
+        setLoading(false)
+      }
     }
+    getFeeds()
+  }, [])
 
-    useEffect(() => {
-        setLoading(true);
-        const getFeeds = () => {
-            type CustomFeed = {}
-            type CustomItem = { description: string }
-            const parser: Parser<CustomFeed, CustomItem> = new Parser({
-                customFields: {
-                    item: ['description']
-                }
-            })
-            parser.parseURL("https://proxy.knulst.de/medium", function (err, feed) {
-                const data = []
-                feed.items.forEach(item => {
-                    const image = item.description.substring(item.description.indexOf('https://cdn-images'), item.description.indexOf('" width'))
-                    data.push({
-                        title: item.title,
-                        link: item.link.substring(0, item.link.indexOf('?')),
-                        snippet: item.contentSnippet,
-                        description: item.description,
-                        image: image,
-                        pubDate: item.pubDate
-                    })
-                })
-                setData(data)
-                setLoading(false);
-            })
-        };
-        getFeeds()
-    }, [])
-
-    return (
-        <div className="blog">
-            <h1 className="text-4xl font-extrabold text-gray-800 dark:text-white p-2 text-center">
-                Blog
-            </h1>
-            <div className="flex flex-col items-center justify-center py-2">
-                <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-                    {loading && Array(10).fill(0, 0).map(() => (<SkeletonBlogEntry/>))}
-                    {!loading &&
-                        data.map((d, i) => (
-                            <button
-                                key={i}
-                                onClick={() => {
-                                    openInNewTab(d.link)
-                                }}
-                                className="transition ease-in-out hover:-translate-y-1 hover:scale-110 duration-300 ...">
-                                <div className="w-96 p-1">
-                                    <div className="px-5 text-left">{d.pubDate}</div>
-                                    <div
-                                        className="px-6 py-3 text-base bg-gray-dark"
-                                    >
-                                        <div className="relative mb-4 rounded-lg text-[0px]">
-                                            <img src={d.image} alt={d.t}/>
-                                        </div>
-                                        <h2 className="text-lg font-bold hover:text-sky-500 dark:text-white">{d.title}</h2>
-                                    </div>
-                                </div>
-                            </button>
-                        ))
-                    }
+  return (
+    <div className="blog">
+      <h1 className="p-2 text-center text-4xl font-extrabold text-gray-800 dark:text-white">
+        Blog
+      </h1>
+      <div className="flex flex-col items-center justify-center py-2">
+        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
+          {loading &&
+            Array(10)
+              .fill(0, 0)
+              .map(() => <SkeletonBlogEntry />)}
+          {!loading &&
+            data.map((d, i) => (
+              <button
+                key={i}
+                onClick={() => {
+                  openInNewTab(d.link)
+                }}
+                className="... transition duration-300 ease-in-out hover:-translate-y-1 hover:scale-110"
+              >
+                <div className="w-96 p-1">
+                  <div className="px-5 text-left">{d.pubDate}</div>
+                  <div className="bg-gray-dark px-6 py-3 text-base">
+                    <div className="relative mb-4 rounded-lg text-[0px]">
+                      <img src={d.image} alt={d.t} />
+                    </div>
+                    <h2 className="text-lg font-bold hover:text-sky-500 dark:text-white">
+                      {d.title}
+                    </h2>
+                  </div>
                 </div>
-            </div>
+              </button>
+            ))}
         </div>
-    );
-};
+      </div>
+    </div>
+  )
+}
 
-export default Blog;
+export default Blog
